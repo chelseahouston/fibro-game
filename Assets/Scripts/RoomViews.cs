b35223628aa2494a03b25c57d169ef88b89602e4
Chelsea Houston <chelseahouston29@gmail.com>
2023-11-15 09:06:44 +0000

    hallway added switch rooms logic added

--- Assets/Scripts/RoomViews.cs
+++ Assets/Scripts/RoomViews.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // author: chelsea houston
 // date last modified: 14/11/23
@@ -11,6 +12,7 @@ public class RoomViews : MonoBehaviour
     public int currentViewIndex;
     public CameraMovement camMovement;
     public GameObject leftArrow, rightArrow;
+    public string currentScene, previousScene;
 
     private void Awake()
     {
@@ -19,14 +21,38 @@ public class RoomViews : MonoBehaviour
         {
             view.SetActive(false);
         } // set all to inactive
-        currentViewIndex = 1; // always load bedroom on room angle 2 (index 1, door enrtry view) - conditionals needed
-        views[currentViewIndex].SetActive(true); // show first angle
     }
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        LoadRoom();
         camMovement.CameraIn();
+    }
+
+    public void LoadRoom()
+    {
+
+        currentScene = SceneManager.GetActiveScene().name;
+
+        switch (currentScene, previousScene)
+        {
+            case ("Bedroom", "Hallway"): // when coming from the hallway always load on room angle 2
+                currentViewIndex = 1; // room angle 2 (index 1, door enrtry view)
+                break;
+
+            case ("Hallway", "Bedroom"): // when coming from the bedroom to the hallways always load on room angle 2
+                currentViewIndex = 1; // room angle 2 (index 1, bedroom to hallway entry view)
+                break;
+
+        }
+
+        views[currentViewIndex].SetActive(true); // show first angle as set above
+    }
+
+    public void SetPreviousScene() // called from door OnClick methods before the scene change
+    {
+        previousScene = currentScene;
     }
 
     // rotate room to the right
@@ -71,9 +97,25 @@ public class RoomViews : MonoBehaviour
 
     public void ExitRoom()
     {
+        SetPreviousScene();
         DisableArrows();
         camMovement.CameraOut();
+    }
 
+    public void LoadBedroom()
+    {
+        StartCoroutine(LoadScene("Bedroom"));
+    }
+
+    public void LoadHallway()
+    {
+        StartCoroutine(LoadScene("Hallway"));
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f); // time for the last scene to move out
+        SceneManager.LoadScene(sceneName);
     }
 
 
