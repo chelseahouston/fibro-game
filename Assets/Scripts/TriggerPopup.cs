e395e09e3d1e98f0d56cc0cb90973a9a1ffb4016
Chelsea Houston <chelseahouston29@gmail.com>
2023-12-11 14:46:41 +0000

    tv

---
+++ Assets/Scripts/TriggerPopup.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TriggerPopup : MonoBehaviour
+{
+    // Dictionary to store tag-text mappings
+    private Dictionary<string, string> actionlist = new Dictionary<string, string>();
+    public TextMeshProUGUI popupText;
+    public GameObject popup;
+
+    // Initialize the dictionary in Start or Awake method
+    void Start()
+    {
+        PopulateActionTagDictionary();
+        popup.SetActive(false);
+    }
+
+    private void PopulateActionTagDictionary()
+    {
+        actionlist.Add("TV", "Watch TV");
+        actionlist.Add("Bed", "Sleep");
+        actionlist.Add("Fridge", "Cook Food");
+        actionlist.Add("Sofa", "Sit");
+        actionlist.Add("ArmChair", "Sit");
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D thing)
+    {
+        Debug.Log("Entered Object Tag: " + thing.tag);
+
+        // Check if the object has a tag in the dictionary
+        if (actionlist.ContainsKey(thing.tag))
+        {
+            popup.SetActive(true);
+            // get the corresponding text from the dictionary
+            popupText.text = actionlist[thing.tag];
+            // display the popup text
+            Debug.Log("Popup Text: " + popupText.text);
+            
+        }
+    }
+
+    public void OnTriggerExit2D()
+    {
+        popupText.text = "";
+        popup.SetActive(false);
+    }
+}
