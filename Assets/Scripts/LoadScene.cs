66035ed0ca080a21015f215ce9f71447fd33e0e8
Chelsea Houston <chelseahouston29@gmail.com>
2023-12-11 15:23:22 +0000

    door goes outside scene

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneName;
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D thing)
     {
-        
+        sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "Home" & thing.tag == "HomeDoor") // if front door but inside house, go outside
+        {
+            SceneManager.LoadScene("Outside");
+        }
+
+        if (sceneName == "Outside" & thing.tag == "HomeDoor") // if front door but outside house, go intside
+        {
+            SceneManager.LoadScene("Home");
+        }
     }
+    
+
+
 }
