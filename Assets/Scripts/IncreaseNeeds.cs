1f6085493c78ea803aae186bc9975b348c3d37a1
Chelsea Houston <chelseahouston29@gmail.com>
2024-01-09 08:12:29 +0000

    E key triggers increase needs

--- Assets/Scripts/IncreaseNeeds.cs
+++ Assets/Scripts/IncreaseNeeds.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class IncreaseNeeds : MonoBehaviour
 {
     public NeedsManager needsManager;
+    private bool triggered;
+    public new string tag;
 
+
+    private void Update()
+    {
+        // if action button pressed
+        if (Input.GetKeyDown(KeyCode.E))
+
+        { // if within trigger area of an object
+            if (triggered)
+            {
+                // increase the needs for that object
+                needsManager.IncreaseNeeds(tag);
+                triggered = false; // reset bool
+            }
+        }
+    }
     public void OnTriggerEnter2D(Collider2D thing)
     {
+        // if it doesnt hve a trigger collider, don't do anything
         if (!thing.isTrigger)
         {
             return;
         }
-
-        needsManager.IncreaseNeeds(thing.tag);
+        else
+        {
+            triggered = true;
+            tag = thing.tag;
+            // set tag for use in Update method to increase needs for the item
+        }
     }
 }
